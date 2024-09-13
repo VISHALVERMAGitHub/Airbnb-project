f3289a3db7b3c431482b94a5b872d582fda60ab9
Vishal Verma <vishalvermabst9521@gmail.com>
2024-09-13 10:11:57 +0530

    add MVC ,add controller for listing , review , user and use router.route in routes/user.js

---
+++ controller/user.js
@@ -0,0 +1,46 @@
+const User =require("../models/user");
+
+module.exports.renderSignInform = (req,res)=>{
+    res.render("users/signup.ejs");
+}
+module.exports.userSignIn = async (req,res)=>{
+    try{
+       let {username,email,password}=req.body;
+       let newuser = new User({email,username});
+       let registeredUser = await User.register(newuser,password);
+       console.log(registeredUser);
+       req.login(registeredUser,(err)=>{
+          if(err){
+             return next(err);
+          }
+          req.flash("success","Welcome to Wanderlust")
+          res.redirect("/listings");
+       })
+    }catch(e){
+       req.flash("error",e.message);
+       res.redirect("/signup");
+    }
+ }
+
+ module.exports.renderLogInForm = (req,res)=>{
+    res.render("users/login.ejs");
+ }
+
+module.exports.userLogIn = async(req,res)=>{
+    req.flash("success","welcome back to wanderlust!")
+    
+    console.log(res.locals.redirectUrl);
+    let redirectUrl = res.locals.redirectUrl ||"/listings";
+    res.redirect(redirectUrl); 
+ }
+
+ module.exports.userLogout = (req,res)=>{
+    req.logOut((err)=>{
+       if(err){
+          return next(err);
+       }
+       req.flash("success","you are logged out !")
+       res.redirect("/listings");
+    });
+    
+ }
--- routes/review.js
+++ routes/review.js
@@ -1,44 +1,19 @@
 const express=require("express");
 const router = express.Router({mergeParams:true});
-
-const Listing =require("../models/listing.js");
 const asyncWrap = require("../utils/asyncWrap.js");
-const ExpressError = require("../utils/ExpressError.js");
 
-const Review =require("../models/review.js");
 const {validateReview, isloggedIn, isReviewAuthor} = require("../middleware.js")
+const reviewController = require("../controller/review.js");
 
 
 // reviews ka post route
-router.post("/",isloggedIn, validateReview , asyncWrap( async(req,res)=>{
-    let listing=await Listing.findById({_id:req.params.id});
-    let newReview=new Review(req.body.review);
-    listing.reviews.push(newReview);
-    newReview.author=req.user._id ;   // req.user is loggedin user
-    // console.log(newReview);
-    
-    await newReview.save();
-    await listing.save();
-    req.flash("success","New Review Created !")
-    res.redirect(`/listings/${listing._id}`);
-
-})
+router.post("/",isloggedIn, validateReview , asyncWrap( reviewController.createReview)
 );
 
 // delete review route
 
-router.delete("/:reviewId",isloggedIn,isReviewAuthor,async(req,res,next)=>{
-    let {id,reviewId}=req.params;
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success","Review deleted !")
-    res.redirect(`/listings/${id}`); 
-})
-router.get("/:reviewId",isloggedIn,isReviewAuthor,async(req,res,next)=>{
-    let {id,reviewId}=req.params;
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success","Review deleted !")
-    res.redirect(`/listings/${id}`); 
-})
+router.delete("/:reviewId",isloggedIn,isReviewAuthor,reviewController.destroyReview)
+
+// router.get("/:reviewId",isloggedIn,isReviewAuthor,reviewController.getRequestDeletionReviewBeforeLogin)
+
 module.exports=router;
